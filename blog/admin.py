17f0b630889d6b29c80b6ae971453624e9a6555c
yunus.toprak12@hotmail.com <yunus.toprak12@hotmail.com>
2017-11-27 21:26:32 +0300

    Django Boys uygulama, ikinci commit

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+admin.site.register(models.Ogrenciler)
+admin.site.register(models.Ogrenciler2)
+admin.site.register(models.Post)
